183563898c1111e04898c640f327a2e42e1acc3b
dante choi <openingan@gmail.com>
2025-04-23 00:14:26 +0900

    환율 데이터 cache용 저장을 위해 dynamodb 사용 (redis는 free tier로 계속 사용불가(12개월 이후 과금)하여 dynamodb로 사용)

--- apps/backend/lib/backend-stack.ts
+++ apps/backend/lib/backend-stack.ts
@@ -2,46 +2,43 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigatewayv2 from 'aws-cdk-lib/aws-apigatewayv2';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as integrations from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 import * as path from 'path';
-import { DockerImage } from 'aws-cdk-lib';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const lambdaPath = path.join(__dirname, '../lambda');
 
-    const handler = new lambda.Function(this, 'HelloHandler', {
+    const lambdaFn = new NodejsFunction(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_18_X,
-      handler: 'hello.handler',
-      code: lambda.Code.fromAsset(lambdaPath, {
-        bundling: {
-          local: {
-            tryBundle(outputDir: string) {
-              const { execSync } = require('child_process');
-              execSync(
-                `npx esbuild hello.ts --bundle --platform=node --target=node18 --outfile=${outputDir}/hello.js`,
-                {
-                  cwd: lambdaPath,
-                  stdio: 'inherit',
-                }
-              );
-              return true;
-            },
-          },
-          image: DockerImage.fromRegistry('node:18-alpine'),
-        },
-      }),
+      entry: path.join(__dirname, '../lambda/hello.ts'),
+      handler: 'handler', // export된 함수 이름
+      bundling: {
+        target: 'node18',
+        platform: 'node',
+        minify: false,
+        externalModules: ['aws-sdk'], // lambda 런타임에 이미 있는 거 제외
+      },
     });
 
 
+    const table = new dynamodb.Table(this, 'ExchangeRates', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+    table.grantReadWriteData(lambdaFn);
+
     const httpApi = new apigatewayv2.HttpApi(this, 'HttpApi', {
-      defaultIntegration: new integrations.HttpLambdaIntegration('LambdaIntegration', handler),
+      defaultIntegration: new integrations.HttpLambdaIntegration('LambdaIntegration', lambdaFn),
     });
 
     new cdk.CfnOutput(this, 'HttpApiUrl', {
       value: httpApi.url ?? 'Something went wrong',
     });
+
   }
 }
