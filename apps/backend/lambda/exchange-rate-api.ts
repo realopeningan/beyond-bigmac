23f2663423e764412d9cf62eb27cd3d0c1fe99d8
dante choi <openingan@gmail.com>
2025-05-04 12:04:59 +0900

    환율정보 가져오는 API 추가

--- apps/backend/lambda/exchange-rate-api.ts
+++ apps/backend/lambda/exchange-rate-api.ts
@@ -1,6 +1,38 @@
+import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
+
+const ddb = new DynamoDBClient({});
+const tableName = process.env.TABLE_NAME!;
+
 export const handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from Dante Playground' }),
-  };
+  try {
+    const { Item } = await ddb.send(
+      new GetItemCommand({
+        TableName: tableName,
+        Key: {
+          id: { S: 'latest' },
+        },
+      })
+    );
+
+    if (!Item) {
+      throw new Error('No exchange rate data found.');
+    }
+
+    const response = {
+      timestamp: Number(Item.timestamp.N),
+      base: Item.base.S,
+      rates: JSON.parse(Item.rates.S as string),
+    };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (err: any) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error fetching exchange rate', error: err.message }),
+    };
+  }
 };
